bd4689cb7376796ccfe3efa2e34c36e2c0de9d0e
Thomas Peuß <thomas@peuss.de>
2024-02-25 15:52:29 +0100

    Some styling

--- Pico_MKS_TS35_R/src/UI.cpp
+++ Pico_MKS_TS35_R/src/UI.cpp
@@ -47,6 +47,7 @@ void init_state_ui() {
 
 lv_span_t *axis_value_span[3];
 lv_style_t axis_style;
+lv_style_t axis_group_style;
 
 void init_axis_ui() {
     lv_style_init(&axis_style);
@@ -59,10 +60,23 @@ void init_axis_ui() {
     lv_style_set_text_align(&axis_style, LV_TEXT_ALIGN_RIGHT);
     lv_style_set_bg_opa(&axis_style, LV_OPA_50);
     lv_style_set_bg_color(&axis_style, lv_palette_main(LV_PALETTE_BLUE));
-    lv_style_set_width(&axis_style, LV_PCT(50));
+    lv_style_set_width(&axis_style, LV_PCT(100));
+
+    lv_style_init(&axis_group_style);
+    lv_style_set_pad_row(&axis_group_style, 0);
+    lv_style_set_pad_column(&axis_group_style, 0);
+    lv_style_set_pad_all(&axis_group_style, 0);
+
+    auto axis_group = lv_obj_create(base_obj);
+    lv_obj_add_flag(axis_group, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
+    lv_obj_set_width(axis_group, LV_PCT(50));
+    lv_obj_set_flex_flow(axis_group, LV_FLEX_FLOW_COLUMN);
+
+    lv_obj_add_style(axis_group, &axis_group_style, LV_PART_MAIN);
 
     for (int i = 0; i < 3; ++i) {
-        auto span_grp = lv_spangroup_create(base_obj);
+        auto span_grp = lv_spangroup_create(axis_group);
+
         lv_obj_add_style(span_grp, &axis_style, LV_PART_MAIN);
 
         auto axis_label_span = lv_spangroup_new_span(span_grp);
@@ -80,11 +94,12 @@ void init_base() {
     lv_style_init(&base_style);
     lv_style_set_pad_row(&base_style, 0);
     lv_style_set_pad_column(&base_style, 0);
-    lv_obj_set_flex_flow(base_obj, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_flex_flow(base_obj, LV_FLEX_FLOW_ROW);
     lv_obj_add_style(base_obj, &base_style, LV_PART_MAIN);
 }
 
 lv_style_t jogging_style;
+lv_style_t jogging_button_style;
 
 static int32_t col_dsc[] = {LV_PCT(33), LV_PCT(33), LV_PCT(33), LV_GRID_TEMPLATE_LAST};
 static int32_t row_dsc[] = {LV_PCT(33), LV_PCT(33), LV_PCT(33), LV_GRID_TEMPLATE_LAST};
@@ -92,6 +107,12 @@ static int32_t row_dsc[] = {LV_PCT(33), LV_PCT(33), LV_PCT(33), LV_GRID_TEMPLATE
 void init_jogging_ui() {
     lv_style_init(&jogging_style);
     lv_style_set_align(&jogging_style, LV_ALIGN_RIGHT_MID);
+    lv_style_set_width(&jogging_style, LV_PCT(50));
+    lv_style_set_pad_all(&jogging_style, 0);
+
+    lv_style_init(&jogging_button_style);
+    lv_style_set_width(&jogging_button_style, LV_PCT(33));
+    lv_style_set_pad_all(&jogging_button_style, 0);
 
     auto jogging_grid = lv_obj_create(base_obj);
     lv_obj_set_style_grid_column_dsc_array(jogging_grid, col_dsc, 0);
@@ -108,6 +129,7 @@ void init_jogging_ui() {
         uint8_t row = i / 3;
 
         obj = lv_button_create(jogging_grid);
+        lv_obj_add_style(obj, &jogging_button_style, LV_PART_MAIN);
         /*Stretch the cell horizontally and vertically too
          *Set span to 1 to make the cell 1 column/row sized*/
         lv_obj_set_grid_cell(obj, LV_GRID_ALIGN_STRETCH, col, 1,
